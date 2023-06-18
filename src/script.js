tremby/mementophobia

commit c7c0b5e3ef6a0d31edcebe5ef496f7f75e3d01ee

Shade speed chart areas only where the speed is possible
Rather than areas between the ghost's minimum and maximum speed. For
example, twins can't be between slow base speed and fast base speed when
there's no line of sight.

Closes #10

src/script.js
@@ -860,6 +860,20 @@ function formatTemperature(tempC) {
 	return getTemperatureFormatter().format(temperatureInPreferredUnit(tempC));
 }
 
+function slowestOf(ghost) {
+	let slowest = null;
+	for (const speed of ghost.speeds.flat())
+		if (slowest == null || speed.speed < slowest.speed) slowest = speed;
+	return slowest;
+}
+
+function fastestOf(ghost) {
+	let fastest = null;
+	for (const speed of ghost.speeds.flat())
+		if (fastest == null || speed.speed > fastest.speed) fastest = speed;
+	return fastest;
+}
+
 function getSpeedMarkers() {
 	const speedMarkers = [];
 
@@ -871,7 +885,7 @@ function getSpeedMarkers() {
 		const speeds = [];
 		if (getLineOfSight() == null) {
 			// Line of sight time unknown; could be the full range of possible speeds
-			speeds.push(
+			speeds.push([
 				{
 					name: "Base",
 					speed: NORMAL_SPEED,
@@ -880,7 +894,7 @@ function getSpeedMarkers() {
 					name: `Max, after at least ${secondsFormatter.format(MAX_GHOST_LOS_SPEEDUP_PERIOD_S)} line of sight`,
 					speed: NORMAL_SPEED * MAX_GHOST_LOS_SPEEDUP_MULTIPLIER,
 				},
-			);
+			]);
 		} else {
 			// Line of sight time known; we can calculate what speed it should be
 			speeds.push({
@@ -919,19 +933,18 @@ function getSpeedMarkers() {
 			});
 		}
 
-		if (someLos !== true) {
-			jinn.speeds.push({
-				name: "Base speed",
-				speed: NORMAL_SPEED,
-			});
-		}
-
-		if (someLos !== false && !(fuseBox && someLos && far)) {
-			if (getLineOfSight() == null || getLineOfSight >= MAX_GHOST_LOS_SPEEDUP_PERIOD_S) {
-				jinn.speeds.push({
-					name: `Max, after at least ${secondsFormatter.format(MAX_GHOST_LOS_SPEEDUP_PERIOD_S)} line of sight`,
-					speed: NORMAL_SPEED * MAX_GHOST_LOS_SPEEDUP_MULTIPLIER,
-				});
+		if (!(fuseBox && someLos && far)) {
+			if (getLineOfSight() == null) {
+				jinn.speeds.push([
+					{
+						name: "Base",
+						speed: NORMAL_SPEED,
+					},
+					{
+						name: `Max, after at least ${secondsFormatter.format(MAX_GHOST_LOS_SPEEDUP_PERIOD_S)} line of sight`,
+						speed: NORMAL_SPEED * MAX_GHOST_LOS_SPEEDUP_MULTIPLIER,
+					},
+				]);
 			} else {
 				jinn.speeds.push({
 					name: `At ${secondsFormatter.format(getLineOfSight())} line of sight`,
@@ -939,6 +952,7 @@ function getSpeedMarkers() {
 				});
 			}
 		}
+
 		speedMarkers.push(jinn);
 	}
 
@@ -1031,22 +1045,26 @@ function getSpeedMarkers() {
 		if (getLineOfSight() == null) {
 			// Line of sight time unknown; could be the full range of possible speeds
 			twins.speeds.push(
-				{
-					name: "Slow twin base",
-					speed: TWIN_SLOW_SPEED,
-				},
-				{
-					name: `Slow twin max, after at least ${secondsFormatter.format(MAX_GHOST_LOS_SPEEDUP_PERIOD_S)} line of sight`,
-					speed: TWIN_SLOW_SPEED * MAX_GHOST_LOS_SPEEDUP_MULTIPLIER,
-				},
-				{
-					name: "Fast twin base",
-					speed: TWIN_FAST_SPEED,
-				},
-				{
-					name: `Fast twin max, after at least ${secondsFormatter.format(MAX_GHOST_LOS_SPEEDUP_PERIOD_S)} line of sight`,
-					speed: TWIN_FAST_SPEED * MAX_GHOST_LOS_SPEEDUP_MULTIPLIER,
-				},
+				[
+					{
+						name: "Slow twin base",
+						speed: TWIN_SLOW_SPEED,
+					},
+					{
+						name: `Slow twin max, after at least ${secondsFormatter.format(MAX_GHOST_LOS_SPEEDUP_PERIOD_S)} line of sight`,
+						speed: TWIN_SLOW_SPEED * MAX_GHOST_LOS_SPEEDUP_MULTIPLIER,
+					},
+				],
+				[
+					{
+						name: "Fast twin base",
+						speed: TWIN_FAST_SPEED,
+					},
+					{
+						name: `Fast twin max, after at least ${secondsFormatter.format(MAX_GHOST_LOS_SPEEDUP_PERIOD_S)} line of sight`,
+						speed: TWIN_FAST_SPEED * MAX_GHOST_LOS_SPEEDUP_MULTIPLIER,
+					},
+				],
 			);
 		} else {
 			// Line of sight time known; we can calculate what speed it should be
@@ -1077,7 +1095,7 @@ function getSpeedMarkers() {
 		if (nearElectronics !== true) {
 			if (getLineOfSight() == null) {
 				// Line of sight time unknown; could be the full range of possible speeds
-				raiju.speeds.push(
+				raiju.speeds.push([
 					{
 						name: "Base, not near active electronics",
 						speed: NORMAL_SPEED,
@@ -1086,7 +1104,7 @@ function getSpeedMarkers() {
 						name: `Max, not near active electronics, after at least ${secondsFormatter.format(MAX_GHOST_LOS_SPEEDUP_PERIOD_S)} line of sight`,
 						speed: NORMAL_SPEED * MAX_GHOST_LOS_SPEEDUP_MULTIPLIER,
 					},
-				);
+				]);
 			} else {
 				// Line of sight time known; we can calculate what speed it should be
 				raiju.speeds.push({
@@ -1115,7 +1133,7 @@ function getSpeedMarkers() {
 		};
 		const sanity = getSanity();
 		if (sanity == null && losMultiplier == null) {
-			moroi.speeds.push(
+			moroi.speeds.push([
 				{
 					name: `At ${percentFormatter.format(MOROI_MIN_AT_SANITY)} or higher sanity, and no line of sight`,
 					speed: MOROI_MIN_SPEED,
@@ -1124,9 +1142,9 @@ function getSpeedMarkers() {
 					name: `At ${percentFormatter.format(MOROI_MAX_AT_SANITY)} sanity, after at least ${secondsFormatter.format(MAX_GHOST_LOS_SPEEDUP_PERIOD_S)} line of sight`,
 					speed: MOROI_MAX_SPEED * MAX_GHOST_LOS_SPEEDUP_MULTIPLIER,
 				},
-			);
+			]);
 		} else if (sanity == null) {
-			moroi.speeds.push(
+			moroi.speeds.push([
 				{
 					name: `At ${percentFormatter.format(MOROI_MIN_AT_SANITY)} or higher sanity, with ${secondsFormatter.format(getLineOfSight())} line of sight`,
 					speed: MOROI_MIN_SPEED * losMultiplier,
@@ -1135,11 +1153,11 @@ function getSpeedMarkers() {
 					name: `At ${percentFormatter.format(MOROI_MAX_AT_SANITY)} sanity, with ${secondsFormatter.format(getLineOfSight())} line of sight`,
 					speed: MOROI_MAX_SPEED * losMultiplier,
 				},
-			);
+			]);
 		} else {
 			const sanitySpeed = Math.max(MOROI_MIN_SPEED, MOROI_MAX_SPEED + sanity * (MOROI_MIN_SPEED - MOROI_MAX_SPEED) / (MOROI_MIN_AT_SANITY - MOROI_MAX_AT_SANITY));
 			if (losMultiplier == null) {
-				moroi.speeds.push(
+				moroi.speeds.push([
 					{
 						name: `At ${percentFormatter.format(sanity)} sanity, with no line of sight`,
 						speed: sanitySpeed,
@@ -1148,7 +1166,7 @@ function getSpeedMarkers() {
 						name: `At ${percentFormatter.format(sanity)} sanity, after at least ${secondsFormatter.format(MAX_GHOST_LOS_SPEEDUP_PERIOD_S)} line of sight`,
 						speed: sanitySpeed * MAX_GHOST_LOS_SPEEDUP_MULTIPLIER,
 					},
-				);
+				]);
 			} else {
 				moroi.speeds.push({
 					name: `At ${percentFormatter.format(sanity)} sanity, with ${secondsFormatter.format(getLineOfSight())} line of sight`,
@@ -1190,7 +1208,7 @@ function getSpeedMarkers() {
 				speed: DEOGEN_MAX_SPEED,
 			};
 			const distance = getGhostDistance();
-			if (distance == null) deogen.speeds.push(slowSpeed, fastSpeed);
+			if (distance == null) deogen.speeds.push([slowSpeed, fastSpeed]);
 			else if (distance <= DEOGEN_MIN_DISTANCE) deogen.speeds.push(slowSpeed);
 			else if (distance >= DEOGEN_MAX_DISTANCE) deogen.speeds.push(fastSpeed);
 			else deogen.speeds.push({
@@ -1218,36 +1236,30 @@ function getSpeedMarkers() {
 		};
 		const ageLimit = Math.ceil((THAYE_MAX_SPEED - THAYE_MIN_SPEED) / THAYE_SPEED_NERF_PER_AGE);
 		if (timeSpentNearGhost == null)
-			thaye.speeds.push(minSpeedObj, maxSpeedObj);
+			thaye.speeds.push([minSpeedObj, maxSpeedObj]);
 		else {
 			const minAges = Math.floor(timeSpentNearGhost / THAYE_AGE_MAX_S);
 			const maxAges = Math.floor(timeSpentNearGhost / THAYE_AGE_MIN_S);
+			const speedrange = [];
 			if (minAges === 0)
-				thaye.speeds.push(minSpeedObj);
+				speedrange.push(minSpeedObj);
 			else
-				thaye.speeds.push({
+				speedrange.push({
 					name: `After ageing ${minAges} times (players have spent between ${minutesFormatter.format(minAges * THAYE_AGE_MIN_S / 60)} and ${minutesFormatter.format(minAges * THAYE_AGE_MAX_S / 60)} in the same room as the ghost)`,
 					speed: Math.max(THAYE_MIN_SPEED, THAYE_MAX_SPEED - minAges * THAYE_SPEED_NERF_PER_AGE),
 				});
 			if (minAges !== maxAges) {
 				if (maxAges === ageLimit)
-					thaye.speeds.push(maxSpeedObj);
+					speedrange.push(maxSpeedObj);
 				else
-					thaye.speeds.push({
+					speedrange.push({
 						name: `After ageing ${maxAges} times (players have spent between ${minutesFormatter.format(maxAges * THAYE_AGE_MIN_S / 60)} and ${minutesFormatter.format(maxAges * THAYE_AGE_MAX_S / 60)} in the same room as the ghost)`,
 						speed: Math.max(THAYE_MIN_SPEED, THAYE_MAX_SPEED - maxAges * THAYE_SPEED_NERF_PER_AGE),
 					});
 			}
+			thaye.speeds.push(speedrange);
 		}
 		speedMarkers.push(thaye);
-	}
-
-	let slowest = null;
-	let fastest = null;
-
-	// Sort all speeds
-	for (const ghost of speedMarkers) {
-		ghost.speeds.sort((a, b) => a.speed - b.speed);
 	}
 
 	// We do the mimic last, since it could be the speed of any other ghost
@@ -1257,20 +1269,37 @@ function getSpeedMarkers() {
 			speeds: [],
 		};
 
+		let slowestSpeed = null;
+		let slowestGhost = null;
+		let fastestSpeed = null;
+		let fastestGhost = null;
+
 		// Find lowest and highest speed of all ghosts
 		for (const ghost of speedMarkers) {
-			if (slowest == null || ghost.speeds[0].speed < slowest.speed) slowest = {
-				name: `Mimic of ${ghost.name}, ${ghost.speeds[0].name}`,
-				speed: ghost.speeds[0].speed,
-			};
-			if (fastest == null || ghost.speeds[ghost.speeds.length - 1].speed > fastest.speed) fastest = {
-				name: `Mimic of ${ghost.name}, ${ghost.speeds[ghost.speeds.length - 1].name}`,
-				speed: ghost.speeds[ghost.speeds.length - 1].speed,
-			};
+			const slowestOfGhost = slowestOf(ghost);
+			if (slowestSpeed == null || slowestOfGhost.speed < slowestSpeed.speed) {
+				slowestSpeed = slowestOfGhost;
+				slowestGhost = ghost;
+			}
+			const fastestOfGhost = fastestOf(ghost);
+			if (fastestSpeed == null || fastestOfGhost.speed > fastestSpeed.speed) {
+				fastestSpeed = fastestOfGhost;
+				fastestGhost = ghost;
+			}
 		}
-		if (slowest != null) mimic.speeds.push(slowest);
-		if (fastest != null) mimic.speeds.push(fastest);
-		if (slowest != null || fastest != null) speedMarkers.push(mimic);
+
+		mimic.speeds.push([
+			{
+				name: `Mimic of ${slowestGhost.name}, ${slowestSpeed.name}`,
+				speed: slowestSpeed.speed,
+			},
+			{
+				name: `Mimic of ${fastestGhost.name}, ${fastestSpeed.name}`,
+				speed: fastestSpeed.speed,
+			},
+		]);
+
+		speedMarkers.push(mimic);
 	}
 
 	return speedMarkers;
@@ -1295,16 +1324,31 @@ function updateSpeedMarkers() {
 		const markersContainer = ghostContainer.querySelector(".markers");
 		markersContainer.replaceChildren(...ghost.speeds.map((speed) => {
 			const div = document.createElement("div");
-			div.style.bottom = `${toScale(tempoFromSpeed(speed.speed)) * 100}%`;
-			div.title = speed.name;
+			if (Array.isArray(speed)) {
+				const range = document.createElement("div");
+				div.appendChild(range);
+				range.classList.add("continuous-range");
+				range.style.bottom = `${toScale(tempoFromSpeed(slowestOf(ghost).speed)) * 100}%`;
+				range.style.height = `${toScale(tempoFromSpeed(fastestOf(ghost).speed) - tempoFromSpeed(slowestOf(ghost).speed)) * 100}%`;
+				for (const speedChild of speed) {
+					const marker = document.createElement("div");
+					div.appendChild(marker);
+					marker.classList.add("marker");
+					marker.style.bottom = `calc(${toScale(tempoFromSpeed(speedChild.speed)) * 100}% + var(--bottom-offset))`;
+					marker.title = speedChild.name;
+				}
+			} else {
+				div.classList.add("marker");
+				div.style.bottom = `calc(${toScale(tempoFromSpeed(speed.speed)) * 100}% + var(--bottom-offset))`;
+				div.title = speed.name;
+			}
 			return div;
 		}));
 		const label = ghostContainer.querySelector(".label");
-		label.style.bottom = `${toScale((tempoFromSpeed(ghost.speeds[0].speed) + tempoFromSpeed(ghost.speeds[ghost.speeds.length - 1].speed)) / 2) * 100}%`;
-		const range = ghostContainer.querySelector(".range");
-		const rangeArea = ghostContainer.querySelector(".range-area");
-		range.style.bottom = rangeArea.style.bottom = `${toScale(tempoFromSpeed(ghost.speeds[0].speed)) * 100}%`;
-		range.style.height = rangeArea.style.height = `${toScale(tempoFromSpeed(ghost.speeds[ghost.speeds.length - 1].speed) - tempoFromSpeed(ghost.speeds[0].speed)) * 100}%`;
+		label.style.bottom = `${toScale((tempoFromSpeed(slowestOf(ghost).speed) + tempoFromSpeed(fastestOf(ghost).speed)) / 2) * 100}%`;
+		const entireRange = ghostContainer.querySelector(".entire-range");
+		entireRange.style.bottom = `${toScale(tempoFromSpeed(slowestOf(ghost).speed)) * 100}%`;
+		entireRange.style.height = `${toScale(tempoFromSpeed(fastestOf(ghost).speed) - tempoFromSpeed(slowestOf(ghost).speed)) * 100}%`;
 	}
 }
 

src/index.html
@@ -1993,8 +1993,7 @@
 
 					<div id="chart">
 						<div class="ghost" data-ghost="spirit">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Spirit
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2002,8 +2001,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="wraith">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Wraith
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2011,8 +2009,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="phantom">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Phantom
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2020,8 +2017,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="poltergeist">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Poltergeist
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2029,8 +2025,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="banshee">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Banshee
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2038,8 +2033,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="jinn">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Jinn
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2047,8 +2041,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="mare">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Mare
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2056,8 +2049,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="revenant">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Revenant
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2065,8 +2057,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="shade">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Shade
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2074,8 +2065,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="demon">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Demon
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2083,8 +2073,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="yurei">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Yurei
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2092,8 +2081,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="oni">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Oni
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2101,8 +2089,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="yokai">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Yokai
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2110,8 +2097,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="hantu">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Hantu
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2119,8 +2105,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="goryo">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Goryo
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2128,8 +2113,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="myling">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Myling
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2137,8 +2121,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="onryo">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Onryo
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2146,8 +2129,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="the-twins">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								The Twins
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2155,8 +2137,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="raiju">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Raiju
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2164,8 +2145,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="obake">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Obake
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2173,8 +2153,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="the-mimic">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								The Mimic
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2182,8 +2161,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="moroi">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Moroi
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2191,8 +2169,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="deogen">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Deogen
 								<button type="button" aria-label="Rule this ghost out">&times;</button>
@@ -2200,8 +2177,7 @@
 							<div class="markers"></div>
 						</div>
 						<div class="ghost" data-ghost="thaye">
-							<div class="range-area"></div>
-							<div class="range"></div>
+							<div class="entire-range"></div>
 							<div class="label">
 								Thaye
 								<button type="button" aria-label="Rule this ghost out">&times;</button>

src/style.css
@@ -130,16 +130,7 @@ input[disabled] {
 #chart .ghost .label button:focus-visible {
 	opacity: 1;
 }
-#chart .ghost .range-area {
-	position: absolute;
-	left: 0;
-	width: 100%;
-	background-color: currentColor;
-	opacity: 0.1;
-	mix-blend-mode: screen;
-	z-index: 0;
-}
-#chart .ghost .range {
+#chart .ghost .entire-range {
 	pointer-events: auto;
 	position: absolute;
 	width: 2px;
@@ -147,7 +138,7 @@ input[disabled] {
 	left: calc(0.5rem + var(--index, 0) * (100% - 4em) / 24);
 	z-index: 1;
 }
-#chart .ghost .range::before {
+#chart .ghost .entire-range::before {
 	/* Expanded hit area */
 	position: absolute;
 	content: "";
@@ -156,17 +147,7 @@ input[disabled] {
 	top: 0;
 	height: 100%;
 }
-#chart .ghost .markers > div {
-	position: absolute;
-	left: 0;
-	bottom: 0;
-	width: 100%;
-	height: 2px;
-	background-color: currentColor;
-	pointer-events: auto;
-	z-index: 1;
-}
-#chart .ghost .markers > div::before {
+#chart .marker::before {
 	/* Expanded hit area */
 	position: absolute;
 	content: "";
@@ -175,6 +156,35 @@ input[disabled] {
 	bottom: calc(0px - var(--expanded-hit));
 	width: 100%;
 }
+#chart .continuous-range, #chart .marker {
+	position: absolute;
+	width: 100%;
+	left: 0;
+	bottom: 0;
+}
+#chart .continuous-range {
+	opacity: 0.1;
+	background-color: currentColor;
+	mix-blend-mode: hard-light;
+	z-index: 0;
+}
+@supports (color: color-mix(in srgb, currentColor 40%, transparent)) {
+	#chart .continuous-range {
+		opacity: 1;
+		background-color: transparent;
+		--center-opacity: 2%;
+		--edge-opacity: 8%;
+		background-image: linear-gradient(to bottom, color-mix(in srgb, currentColor var(--edge-opacity), transparent), color-mix(in srgb, currentColor var(--center-opacity), transparent), color-mix(in srgb, currentColor var(--edge-opacity), transparent));
+	}
+}
+#chart .marker {
+	height: 2px;
+	background-color: currentColor;
+	pointer-events: auto;
+	z-index: 1;
+	--bottom-offset: -1px;
+	bottom: var(--bottom-offset);
+}
 #chart .ghost:is(:hover, :focus-within) {
 	z-index: 3;
 }
@@ -182,10 +192,10 @@ input[disabled] {
 	--extra-padding: 0px;
 	border-width: 4px;
 }
-#chart .ghost:is(:hover, :focus-within) .range {
+#chart .ghost:is(:hover, :focus-within) .entire-range {
 	transform: scaleX(200%);
 }
-#chart .ghost:is(:hover, :focus-within) .markers > div {
+#chart .ghost:is(:hover, :focus-within) .marker {
 	transform: scaleY(200%);
 }
 #tap-target:is(:focus, :focus-within) {

commit 3bd4d276b336c18dd2fe571db425915207a2d138

Fix "narrow by tempo"
Broken since c7c0b5e

Fixes #25

src/script.js
@@ -814,8 +814,8 @@ function narrowByTempo() {
 	const speedMarkers = getSpeedMarkers();
 	for (const ghost of speedMarkers) {
 		if (
-			adjustedTempo * (1 + NARROW_BY_TEMPO_LEEWAY) < tempoFromSpeed(ghost.speeds[0].speed)
-			|| adjustedTempo * (1 - NARROW_BY_TEMPO_LEEWAY) > tempoFromSpeed(ghost.speeds[ghost.speeds.length - 1].speed)
+			adjustedTempo * (1 + NARROW_BY_TEMPO_LEEWAY) < tempoFromSpeed(slowestOf(ghost).speed)
+			|| adjustedTempo * (1 - NARROW_BY_TEMPO_LEEWAY) > tempoFromSpeed(fastestOf(ghost).speed)
 		) byId(nameToIdentifier(ghost.name)).classList.add("impossible-by-speed");
 	}
 	updateClearRulingsByTempo();